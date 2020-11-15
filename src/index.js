cffe1d85069f355ee9bc6493bf6f1c1298f55196
mike591 <mikemach123@gmail.com>
2020-11-15 13:08:44 -0800

    more setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import firebase from "firebase/app";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA8Slwa42sGtvwTDzJ1qBStPc-YE7bNHHk",
+  authDomain: "mytodo-d9d21.firebaseapp.com",
+  databaseURL: "https://mytodo-d9d21.firebaseio.com",
+  projectId: "mytodo-d9d21",
+  storageBucket: "mytodo-d9d21.appspot.com",
+  messagingSenderId: "752649031750",
+  appId: "1:752649031750:web:37eecd377541a09adefa47",
+  measurementId: "G-CQVLEWTLX0",
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
